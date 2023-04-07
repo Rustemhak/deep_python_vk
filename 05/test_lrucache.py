fe3406cdbf0cf6beb9f2e2a522e6226b7df744b2
Rustemhak <rustemhak@mail.ru>
2023-04-07 21:34:24 +0300

    edit test module for lrucache

--- 05/test_lrucache.py
+++ 05/test_lrucache.py
@@ -1,100 +1,100 @@
 """Module for testing LRUCache"""
+import unittest
+
 from lrucache import LRUCache
 
 
-def test_base_limit_0():
-    """test LRUCache limit 0"""
-    cache = LRUCache(0)
+class TestLRUCache(unittest.TestCase):
+    "class for test LRUCache"
 
-    cache.set("k1", "val1")
-    cache.set("k2", "val2")
+    def test_base_limit_0(self):
+        """test LRUCache limit 0"""
+        cache = LRUCache(0)
 
-    assert cache.get("k3") is None
-    assert cache.get("k2") is None
-    assert cache.get("k1") is None
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
 
-    cache.set("k3", "val3")
+        self.assertIsNone(cache.get("k3"))
+        self.assertIsNone(cache.get("k2"))
+        self.assertIsNone(cache.get("k1"))
 
-    assert cache.get("k3") is None
-    assert cache.get("k2") is None
-    assert cache.get("k1") is None
+        cache.set("k3", "val3")
 
+        self.assertIsNone(cache.get("k3"))
+        self.assertIsNone(cache.get("k2"))
+        self.assertIsNone(cache.get("k1"))
 
-def test_base_limit_1():
-    """test LRUCache limit 1"""
+    def test_base_limit_1(self):
+        """test LRUCache limit 1"""
 
-    cache = LRUCache(1)
+        cache = LRUCache(1)
 
-    cache.set("k1", "val1")
-    cache.set("k2", "val2")
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
 
-    assert cache.get("k3") is None
-    assert cache.get("k2") == "val2"
-    assert cache.get("k1") is None
+        self.assertIsNone(cache.get("k3"))
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertIsNone(cache.get("k1"))
 
-    cache.set("k3", "val3")
+        cache.set("k3", "val3")
 
-    assert cache.get("k3") == "val3"
-    assert cache.get("k2") is None
-    assert cache.get("k1") is None
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertIsNone(cache.get("k2"))
+        self.assertIsNone(cache.get("k1"))
 
+    def test_base_limit_2(self):
+        """test LRUCache limit 2"""
 
-def test_base_limit_2():
-    """test LRUCache limit 2"""
+        cache = LRUCache(2)
 
-    cache = LRUCache(2)
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
 
-    cache.set("k1", "val1")
-    cache.set("k2", "val2")
+        self.assertIsNone(cache.get("k3"))
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1")
 
-    assert cache.get("k3") is None
-    assert cache.get("k2") == "val2"
-    assert cache.get("k1") == "val1"
+        cache.set("k3", "val3")
 
-    cache.set("k3", "val3")
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertIsNone(cache.get("k2"))
+        self.assertEqual(cache.get("k1"), "val1")
 
-    assert cache.get("k3") == "val3"
-    assert cache.get("k2") is None
-    assert cache.get("k1") == "val1"
+    def test_base_limit_3(self):
+        """test LRUCache limit 3"""
 
+        cache = LRUCache(3)
 
-def test_base_limit_3():
-    """test LRUCache limit 3"""
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
 
-    cache = LRUCache(3)
+        self.assertIsNone(cache.get("k3"))
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1")
 
-    cache.set("k1", "val1")
-    cache.set("k2", "val2")
+        cache.set("k3", "val3")
 
-    assert cache.get("k3") is None
-    assert cache.get("k2") == "val2"
-    assert cache.get("k1") == "val1"
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k1"), "val1")
 
-    cache.set("k3", "val3")
+    def test_count_cache(self):
+        """test count in LRUCache"""
 
-    assert cache.get("k3") == "val3"
-    assert cache.get("k2") == "val2"
-    assert cache.get("k1") == "val1"
+        cache = LRUCache(2)
+        cache.set("a", "a")
+        self.assertEqual(len(cache.cache), 1)
+        cache.set("b", "b")
+        self.assertEqual(len(cache.cache), 2)
+        cache.set("c", "c")
+        self.assertEqual(len(cache.cache), 2)
 
+    def test_insert_twice(self):
+        """test two insert"""
 
-def test_count_cache():
-    """test count in LRUCache"""
+        cache = LRUCache(3)
 
-    cache = LRUCache(2)
-    cache.set("a", "a")
-    assert len(cache.cache) == 1
-    cache.set("b", "b")
-    assert len(cache.cache) == 2
-    cache.set("c", "c")
-    assert len(cache.cache) == 2
+        cache.set("k1", "val1")
+        cache.set("k1", "val1_1")
 
-
-def test_insert_twice():
-    """test two insert"""
-
-    cache = LRUCache(3)
-
-    cache.set("k1", "val1")
-    cache.set("k1", "val1_1")
-
-    assert cache.get("k1") == "val1_1"
+        self.assertEqual(cache.get("k1"), "val1_1")
